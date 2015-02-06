f070cb51011a6ef34bc57eada55b766cd85e7afe
Edward Muller <edwardam@interlix.com>
2015-02-05 16:47:20 -0800

    Add an example app
    Serve the fs once authenticated

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -8,11 +8,13 @@ import (
 )
 
 func main() {
+
 	g := &googlegoauth.Handler{
-		RequireDomain: "heroku.com",
+		RequireDomain: os.Getenv("REQUIRE_DOMAIN"),
 		Key:           os.Getenv("KEY"),
 		ClientID:      os.Getenv("CLIENT_ID"),
 		ClientSecret:  os.Getenv("CLIENT_SECRET"),
+		Handler:       http.FileServer(http.Dir(".")),
 	}
 
 	http.ListenAndServe(":"+os.Getenv("PORT"), g)
